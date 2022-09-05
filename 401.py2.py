4dac251555edd0d24fd4f79914b35c10af069e2f
puttadileepkumarreddy <112851180+puttadileepkumarreddy@users.noreply.github.com>
2022-09-05 14:00:36 +0530

    Add files via upload

---
+++ 401.py2.py
@@ -0,0 +1,6 @@
+import math
+def area_of_the_circle(radius):
+    area=radius**2*math.pi
+    return area
+radius=float(input("please enter the radius of the given circle:"))
+print("the area of the given circle is:",area_of_the_circle(circle))
